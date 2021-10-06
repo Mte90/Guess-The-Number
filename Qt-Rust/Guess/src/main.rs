3b5c63338c711d8203e9ca7ff2ee3958c0c4a603
Ilyas <ilyas95@hotmail.it>
2021-10-07 00:08:56 +0100

    fix: logic & ui

--- Qt-Rust/Guess/src/main.rs
+++ Qt-Rust/Guess/src/main.rs
@@ -1,25 +1,38 @@
-use std::rc::Rc;
-use qt_core::{QBox, QPtr, QString};
+use rand::{self, Rng};
+use cpp_core::{Ptr, Ref, StaticUpcast};
+use qt_core::{qs, slot, ContextMenuPolicy, QBox, QObject, QPoint, SlotNoArgs, QPtr, QString};
 use qt_ui_tools::ui_form;
-use qt_widgets::{QApplication, QPushButton, QWidget};
+use qt_widgets::{
+    QAction, QApplication, QLineEdit, QMenu, QMessageBox, QPushButton, QVBoxLayout, QWidget, SlotOfQPoint, QLabel
+};
+use std::rc::Rc;
+
 
 #[ui_form("../ui/mainwindow.ui")]
 #[derive(Debug)]
 struct Form {
     widget: QBox<QWidget>,
-    guess: QPtr<QPushButton>,
+    ok: QPtr<QPushButton>,
+    number: QPtr<QLineEdit>,
+    output: QPtr<QLabel>,
 }
 
 #[derive(Debug)]
-struct MainWindow {
-    form: Form,
+struct TodoWidget {
+    form: Form
 }
 
-impl MainWindow {
+impl StaticUpcast<QObject> for TodoWidget {
+    unsafe fn static_upcast(ptr: Ptr<Self>) -> Ptr<QObject> {
+        ptr.form.widget.as_ptr().static_upcast()
+    }
+}
+
+impl TodoWidget {
     fn new() -> Rc<Self> {
         unsafe {
-            let this = Rc::new(MainWindow {
-                form: Form::load(),
+            let this = Rc::new(TodoWidget {
+                form: Form::load(),  
             });
             this.init();
             this
@@ -27,23 +40,39 @@ impl MainWindow {
     }
 
     unsafe fn init(self: &Rc<Self>) {
-        self.form.guess.clicked().connect(&self.guess());
+        self.form.ok.clicked().connect(&self.slot_guess());
+        self.form.number.return_pressed().connect(&self.slot_guess());
     }
 
-    fn guess(&self) {
-        println!("Test");
+    #[slot(SlotNoArgs)]
+    fn guess(self: &Rc<Self>) {
+        let mut rng = rand::thread_rng();
+        let the_number = rng.gen_range(1..=10);
+        unsafe {
+            let user_number = self.form.number.text().to_int_0a();
+            if user_number == the_number {
+                println!("Your number is {} and is right!", user_number)
+            }
+            else {
+                println!("Your number is {} but the right was {}!", user_number, the_number)
+            }
+            // let label = QString::new().as_ptr();
+            // label.arg_i64(the_number);
+            //self.form.output.set_text(QString::from_std_str("Test"));
+        }
     }
-
+    
     fn show(self: &Rc<Self>) {
         unsafe {
             self.form.widget.show();
         }
     }
 }
+
 fn main() {
-    QApplication::init(|_| {
-        let window = MainWindow::new();
-        window.show();
-        unsafe { QApplication::exec() }
+    QApplication::init(|_| unsafe {
+        let todo_widget = TodoWidget::new();
+        todo_widget.show();
+        QApplication::exec()
     })
 }
