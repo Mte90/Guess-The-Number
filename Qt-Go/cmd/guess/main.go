8d4cf7076c63fd2b2dda2c9fd2ed136e09fa3b15
Ilyas <ilyas95@hotmail.it>
2021-10-12 23:54:05 +0100

    improvement

--- Qt-Go/cmd/guess/main.go
+++ Qt-Go/cmd/guess/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"os"
 	"fmt"
+	"strconv"
+	"math/rand"
+
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/widgets"
 	"github.com/ilyasmg/Guess-The-Number/Qt-Go/uigen"
@@ -21,19 +24,31 @@ func main() {
 		Widget: widgets.NewQMainWindow(nil, core.Qt__Window),
 	}
 	window.SetupUI(window.Widget)
-	var (
-		ui_ok = widgets.NewQPushButtonFromPointer(window.Widget.FindChild("ok", core.Qt__FindChildrenRecursively).Pointer())
-		ui_number = widgets.NewQLineEditFromPointer(window.Widget.FindChild("number", core.Qt__FindChildrenRecursively).Pointer())
-		ui_output  = widgets.NewQLabelFromPointer(window.Widget.FindChild("output", core.Qt__FindChildrenRecursively).Pointer())
-	)
-	ui_ok.ConnectClicked(func(bool){
-		fmt.Println("Test1")
-		ui_output.SetText("Test")
+	window.Ok = widgets.NewQPushButtonFromPointer(window.Widget.FindChild("ok", core.Qt__FindChildrenRecursively).Pointer())
+	window.Number = widgets.NewQLineEditFromPointer(window.Widget.FindChild("number", core.Qt__FindChildrenRecursively).Pointer())
+	window.Output  = widgets.NewQLabelFromPointer(window.Widget.FindChild("output", core.Qt__FindChildrenRecursively).Pointer())
+	guess := func() {
+		user_number, err := strconv.Atoi(window.Number.Text())
+		var label string
+		if err != nil {
+			min := 0
+    		max := 10
+    		the_number := rand.Intn(max - min) + min
+			if the_number == user_number {
+				window.Output.SetStyleSheet("QLabel { color : green; }")
+				label = fmt.Sprintf("Your number is %d and is right!", user_number)
+			} else {
+				window.Output.SetStyleSheet("QLabel { color : green; }")
+				label = fmt.Sprintf("Your number is %d but the right was %d!", user_number, the_number)
+			}
+		}
+		window.Output.SetText(label)
+	}
+	window.Ok.ConnectClicked(func(_ bool){
+		guess()
 	})
-
-	ui_number.ConnectReturnPressed(func(){
-		fmt.Println("Test2")
-		ui_output.SetText("Test")
+	window.Number.ConnectReturnPressed(func(){
+		guess()
 	})
 	window.Widget.Show()
 	app.Exec()
